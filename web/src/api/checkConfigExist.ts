22556cd3d3df934ed2b20548d24931e4769d9130
Administrator <2267299114@qq.com>
2025-09-25 18:59:34 +0800

    存在性、唯一性、不为空检查前端

---
+++ web/src/api/checkConfigExist.ts
@@ -0,0 +1,25 @@
+// 配置路径字段存在性扫描 API接口
+import { get, post } from '/@/utils/http/axios';
+
+enum URL {
+    # list = '/myapp/admin/checkConfigExist/list',
+    # create = '/myapp/admin/checkConfigExist/create',
+    # update = '/myapp/admin/checkConfigExist/update',
+    # delete = '/myapp/admin/checkConfigExist/delete',
+    # detail = '/myapp/admin/checkConfigExist/detail',
+    list = '/myapp/admin/scanDevUpdate/scanResultlist',
+    create = '/myapp/admin/scanDevUpdate/scanResultcreate',
+    update = '/myapp/admin/scanDevUpdate/scanResultupdate',
+    delete = '/myapp/admin/scanDevUpdate/scanResultdelete',
+    detail = '/myapp/admin/scanDevUpdate/scanResultdetail',
+}
+
+const listApi = async (params: any) => get<any>({ url: URL.list, params: params, data: {}, headers: {'Content-Type': 'application/x-www-form-urlencoded'} });
+const createApi = async (data: any) =>
+    post<any>({ url: URL.create, params: {}, data: data, timeout:20000, headers: { 'Content-Type': 'multipart/form-data;charset=utf-8' } });
+const updateApi = async (params:any, data: any) =>
+    post<any>({ url: URL.update,params: params, data: data, headers: { 'Content-Type': 'multipart/form-data;charset=utf-8' } });
+const deleteApi = async (params: any) => post<any>({ url: URL.delete, params: params, headers: {} });
+const detailApi = async (params: any) => get<any>({ url: URL.detail, params: params, headers: {} });
+
+export { listApi, createApi, updateApi, deleteApi, detailApi };
